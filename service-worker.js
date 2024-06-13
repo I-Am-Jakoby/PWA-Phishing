9abf056cd2cd432b8ddbe74c0fb857f4a6ad1d34
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2024-06-12 21:52:39 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -13,23 +13,33 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
+  // Only process GET requests.
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   if (event.request.url.endsWith('index.html')) {
-    // Don't cache index.html
+    // Don't cache index.html to ensure the user gets the latest version
     event.respondWith(fetch(event.request));
   } else {
     event.respondWith(
-      caches.match(event.request).then(response => {
+      caches.match(event.request)
+      .then(response => {
         if (response) {
           return response;
         }
-
         return fetch(event.request).then(fetchResponse => {
-          return caches.open('v1').then(cache => {
-            cache.put(event.request, fetchResponse.clone()).catch(error => {
+          // Only cache valid responses
+          if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== 'basic') {
+            return fetchResponse;
+          }
+          var responseToCache = fetchResponse.clone();
+          caches.open('v1').then(cache => {
+            cache.put(event.request, responseToCache).catch(error => {
               console.error('Failed to cache fetch response:', error);
             });
-            return fetchResponse;
           });
+          return fetchResponse;
         });
       }).catch(error => {
         console.error('Fetch event failed:', error);
